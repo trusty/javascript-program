556f47bb493a16c57f3836080a163ea889a48095
gouthamsa <gouthamp222@gmail.com>
2020-08-13 21:14:05 +0530

    loop

---
+++ newprice.js
@@ -0,0 +1,32 @@
+const request = require('request')
+const cheerio = require('cheerio')
+const stock_data = ['TCS', 'DRREDDY', 'INDIGO', 'MINDACORP', 'KTKBANK', 'FEDERALBNK', 'SBIN',
+    'MANAPPURAM',
+    'INDUSINDBK',
+    'TATAMETALI',
+    'SOUTHBANK',
+    'IOLCP',
+    'KEC - EQ',
+    'SUBROS',
+    'LT',
+    'CHOLAFIN',
+    'DEEPAKNTR',
+    'TECHM'
+]
+var stock_price = []
+
+
+function price_data() {
+    for (i = 0; i < stock_data.length; i++) {
+        request('https://in.finance.yahoo.com/quote/' + stock_data[i] + '.NS?p=' + stock_data[i] + '.NS&.tsrc=fin-srch', (error, response, html, i) => {
+            if (!error && response.statusCode == 200) {
+                const $ = cheerio.load(html);
+                let price = $('span[class="Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)"]').html()
+                stock_price.push(price)
+                console.log(i);
+            }
+        });
+    }
+}
+
+price_data();
