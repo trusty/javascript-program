8171de062a75a03e8e0c8a337cc2bf9283f0a68d
trusty <prem.sah@gmail.com>
2020-08-16 11:20:14 +0530

    Update newprice.js

--- newprice.js
+++ newprice.js
@@ -1,6 +1,6 @@
 const request = require('request')
 const cheerio = require('cheerio')
-const stock_data = ['TCS', 'DRREDDY', 'INDIGO', 'MINDACORP', 'KTKBANK', 'FEDERALBNK', 'SBIN',
+const stock_quotes = ['TCS', 'DRREDDY', 'INDIGO', 'MINDACORP', 'KTKBANK', 'FEDERALBNK', 'SBIN',
     'MANAPPURAM',
     'INDUSINDBK',
     'TATAMETALI',
@@ -17,15 +17,18 @@ var stock_price = []
 
 
 function price_data() {
-    for (i = 0; i < stock_data.length; i++) {
-        request('https://in.finance.yahoo.com/quote/' + stock_data[i] + '.NS?p=' + stock_data[i] + '.NS&.tsrc=fin-srch', (error, response, html, i) => {
+    for (stock of stock_quotes) {
+        
+        (function (stock) {
+        request('https://in.finance.yahoo.com/quote/' + stock + '.NS?p=' + stock + '.NS&.tsrc=fin-srch', (error, response, html) => {
             if (!error && response.statusCode == 200) {
                 const $ = cheerio.load(html);
                 let price = $('span[class="Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)"]').html()
                 stock_price.push(price)
-                console.log(i);
+                console.log(stock);
             }
         });
+        }(stock));
     }
 }
 
